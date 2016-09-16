9615983fc176b765b5c1d964e46b7633bbd8b86f
shawn <shawn@naturalprofessional.com>
2016-09-15 17:40:18 -0700

    Clean up unused code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,4 @@
-//= require jquery
-//= require jquery_ujs
-
 $(document).ready(function(){
-
-  // var turbolinks = require('turbolinks')
-
   $(".walk-me-home").on("click", function(e){
     e.preventDefault();
     $.ajax({
@@ -20,7 +14,6 @@ $(document).ready(function(){
        .fail(function(serverResponse){
          console.log("Request to send text failed");
        })
-
     // Start drone. Inactive for now since there's no drone to start.
     // $.ajax({
     //   // Jason's server
@@ -50,7 +43,6 @@ $(document).ready(function(){
     .fail(function(serverResponse) {
       console.log(serverResponse);
     });
-
     // Land drone. Inactive for now since there's no drone to land.
     // $.ajax({
     //   // Drone on Jason's server
@@ -68,8 +60,6 @@ $(document).ready(function(){
     function hideSpinner(){
       $(".spinner").hide()
     }
-
-
     // Walk drone. Inactive for now since there's no drone to move along.
     // $.ajax({
     //   // Drone on Jason's server
@@ -86,46 +76,12 @@ $(document).ready(function(){
     // })
   })
 
-
-  $(".help-status").on("click", function(e){
-  //   e.preventDefault();
-  //   $.ajax({
-  //        url: "/sms/text_friend"
-  //      })
-  //      .done(function(serverResponse){
-  //        $('#pac-input').show()
-
-  //        $('.location-input').removeAttr('id')
-  //        $('.walk-me-home').removeAttr('id')
-  //        $('.location-input').attr('id', 'shown-div')
-  //        $('.walk-me-home').attr('id', 'hidden-div')
-  //      })
-  //      .fail(function(serverResponse){
-  //        console.log("Request to send text failed");
-  //      })
-
-  //   $.ajax({
-  //     // Jason's server
-  //     // url: "http://172.16.51.60:3000/start",
-  //     // Eric's server
-  //     url: "http://172.16.50.232:8080/start",
-  //     crossDomain : true
-  //   })
-  //   .done(function(serverResponse){
-  //   })
-  //   .fail(function(serverResponse){
-  //     console.log("Request to start drone failed");
-  //   })
-  })
-
   $('.watch-status').on("click", function(e) {
 
     var address = window.location.href;
     var split_address = address.split('/');
     var event_id = split_address[split_address.length-2];
-
     this_div = this;
-
     $.ajax({
       url: "/events/"+event_id,
       method: "put",
@@ -138,7 +94,5 @@ $(document).ready(function(){
     .fail(function(serverResponse) {
       console.log(serverResponse);
     });
-
   });
-
 });
